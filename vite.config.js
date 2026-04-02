264d0bb536bad340e9b6ef3edef233fe0402a7f9
azharf99 <azharfaturohman29@gmail.com>
2026-04-03 03:46:52 +0700

    Remove unused environment variable for API base URL configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import 'dotenv/config'
 
-const TARGET_URL = process.env.VITE_TARGET_URL;
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
