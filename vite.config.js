7a0903b670753bc66037e2257e0cb3cb20becafa
azharf99 <azharfaturohman29@gmail.com>
2026-04-03 12:22:23 +0700

    Update Vite server proxy configuration to point to local development API URL

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: `https://wa.azharfa.cloud`,
+        target: `http://localhost:8003`,
         changeOrigin: true,
       },
     },
